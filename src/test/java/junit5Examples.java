07e0d20be35163d870d6632be73def3c407b030a
ZhevaD <zhevenkov@gmail.com>
2022-11-22 21:24:16 +0300

    added build gradle

---
+++ src/test/java/junit5Examples.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+public class JUnit5Examples {
+    @Test
+    void firstTest() {
+        System.out.println("###     @Test firstTest() !");
+
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("###     @Test secondTest() !");
+
+    }
+}
