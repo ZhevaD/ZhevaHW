311a6ca45506f96869cde2a50b7f04d82ef852a7
ZhevaD <zhevenkov@gmail.com>
2022-11-24 23:30:05 +0300

    added started make HW

--- src/test/java/TestBoxTests.java
+++ src/test/java/TestBoxTests.java
@@ -2,8 +2,11 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -19,7 +22,7 @@ public class TestBoxTests {
     void fillFromTest() {
         String firstName = "Dmitriy";
         String lastName = "Zheva";
-        String Email = "Zheva@zheva.com";
+        String eMail = "Zheva@zheva.com";
         String mobilePhone = "9216666666";
         String currentAddress = "Russia";
         String bDate = "08 Nov 1990";
@@ -29,7 +32,7 @@ public class TestBoxTests {
 
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(Email);
+        $("#userEmail").setValue(eMail);
         $("#gender-radio-1").doubleClick();
         $("#userNumber").setValue(mobilePhone);
         $("#dateOfBirthInput").click();
@@ -37,20 +40,37 @@ public class TestBoxTests {
         $(".react-datepicker__month-select").click();
         $(".react-datepicker__year-select").selectOption("1990");
         $(".react-datepicker__year-select").click();
-        $(".react-datepicker__day--08").click(); // НАДО решить как выбрать нужный день
-
-        $("#dateOfBirthInput").setValue(bDate);
-        $("#subjectsInput").setValue("Guns");
-        $("#hobbies-checkbox-1").setValue("1");
-//        $("#uploadPicture").click();
+        $(".react-datepicker__day--008").click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/123.png"));
+        $("#subjectsInput").click();
+        $("#subjectsInput").setValue("Comp");
+        $("#subjectsInput").pressEnter();
+        $("#hobbies-checkbox-1").parent().click();
+        $("#hobbies-checkbox-2").parent().click();
+        $("#hobbies-checkbox-3").parent().click();
         $("#currentAddress").setValue(currentAddress);
-//        $("#state").setValue(currentAddress);
-        $("submit").click();
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Panipat")).click();
+        $("#submit").click();
+        //Check-out
+        $(".modal-content").shouldBe(visible);
+        $(".modal-content").shouldHave(text(firstName));
+        $(".modal-content").shouldHave(text(lastName));
+        $(".modal-content").shouldHave(text(eMail));
+        $(".modal-content").shouldHave(text(mobilePhone));
+        $(".modal-content").shouldHave(text("08 November,1990"));
+        $(".modal-content").shouldHave(text("Computer Science"));
+        $(".modal-content").shouldHave(text("Sports, Reading, Music"));
+        $(".modal-content").shouldHave(text("123.png"));
+        $(".modal-content").shouldHave(text("Russia"));
+        $(".modal-content").shouldHave(text("Haryana Panipat"));
+        $("#closeLargeModal").click();
 
 
 
 
-        $("class=modal-body").shouldBe(visible);
 
     }
 }
